866560e0fdb7b676e85331a7c1b1232f64ad1187
LQQ <674129530@qq.com>
2020-11-08 00:22:53 +0800

    d

--- 数据的加载与处理.py
+++ 数据的加载与处理.py
@@ -2,15 +2,14 @@ from __future__ import print_function, division
 import os
 import torch
 import pandas as pd
-from skimage import io,transform
+from skimage import io, transform
 import numpy as np
 import matplotlib.pyplot as plt
-from torch.utils.data import dataset,dataloader
-from torchvision import transforms,utils
+from torch.utils.data import dataset, dataloader
+from torchvision import transforms, utils
 import warnings
 warnings.filterwarnings('ignore')
 plt.ion()
-#读取数据集
-landmarks_frame  =pd.read_csv('data/faces/face_landmarks.csv')
-
-
+# 读取数据集
+landmarks_frame = pd.read_csv('data/faces/face_landmarks.csv')
+n = 65
