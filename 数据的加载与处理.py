088ba829c3d453a8ce03a22e3d51084cacbff7c2
LQQ <674129530@qq.com>
2020-11-08 00:43:06 +0800

    s

--- 数据的加载与处理.py
+++ 数据的加载与处理.py
@@ -8,8 +8,23 @@ import matplotlib.pyplot as plt
 from torch.utils.data import dataset, dataloader
 from torchvision import transforms, utils
 import warnings
+
+def show_landmarks(image,landmarks):
+    plt.imshow(image)
+    plt.scatter(landmarks[:,0],landmarks[:,1],s=10,marker='.',c='r')
+    plt.pause(0.001)
+
 warnings.filterwarnings('ignore')
 plt.ion()
 # 读取数据集
 landmarks_frame = pd.read_csv('data/faces/face_landmarks.csv')
 n = 65
+img_name = landmarks_frame.iloc[n,0]
+landmarks = landmarks_frame.iloc[n,1:].values
+landmarks = landmarks.astype('float').reshape(-1,2)
+print('Image name:{}'.format(img_name))
+print('Landmarks shape:{}'.format(landmarks.shape))
+print('First 4 Landmarks:\n{}'.format(landmarks[:4]))
+plt.figure()
+show_landmarks(io.imread(os.path.join('data/faces/',img_name)),landmarks)
+plt.show()
